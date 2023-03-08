71cac83c17ee92a86d87851145f03d8107f0cb2d
parkgeonwoong <hk77kh@gmail.com>
2023-03-08 14:43:03 +0900

    Feat : 집합 연산 추가

--- data-structures/set/01집합만들기.js
+++ data-structures/set/01집합만들기.js
@@ -52,11 +52,82 @@ class Set {
   }
 
   // 합집합
+  union(otherSet) {
+    const UnionSet = new Set();
+
+    let values = this.values();
+    for (let i = 0; i < values.length; i++) {
+      UnionSet.add(values[i]);
+    }
+    values = otherSet.values();
+    for (let i = 0; i < values.length; i++) {
+      UnionSet.add(values[i]);
+    }
+    return UnionSet;
+  }
+
+  // 교집합
+  intersection(otherSet) {
+    const intersectionSet = new Set();
+    let values = this.values();
+
+    for (let i = 0; i < values.length; i++) {
+      if (otherSet.has(values[i])) {
+        intersectionSet.add(values[i]);
+      }
+    }
+    return intersectionSet;
+  }
+
+  // 차집합
+  difference(otherSet) {
+    const differenceSet = new Set();
+    let values = this.values();
+
+    for (let i = 0; i < values.length; i++) {
+      if (!otherSet.has(values[i])) {
+        differenceSet.add(values[i]);
+      }
+    }
+    return differenceSet;
+  }
+
+  // 부분집합
+  subset(otherSet) {
+    if (this.size() > otherSet.size()) {
+      return false;
+    } else {
+      let values = this.values();
+      for (let i = 0; i < values.length; i++) {
+        if (!otherSet.has(values[i])) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
 }
 
 const set = new Set();
-set.add(1);
-set.add(2);
-console.log(set.size());
-console.log(set.values());
-console.log(set.has(1));
+// set.add(1);
+// set.add(2);
+// console.log(set.size());
+// console.log(set.values());
+// console.log(set.has(1));
+
+const setA = new Set();
+setA.add(1);
+setA.add(2);
+
+const setB = new Set();
+setB.add(2);
+setB.add(3);
+
+const unionAB = setA.union(setB);
+console.log("합집합: ", unionAB.values());
+
+const intersectionAB = setA.intersection(setB);
+console.log("교집합: ", intersectionAB.values());
+
+const differenceAB = setA.difference(setB);
+console.log("차집합: ", differenceAB.values());
