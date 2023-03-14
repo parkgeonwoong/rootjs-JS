180718f5b309e4a24e8ac624cf9e8dfa5b99abe6
parkgeonwoong <hk77kh@gmail.com>
2023-03-14 17:57:50 +0900

    Feat : 깊이우선탐색 이해와 구현

---
+++ data-structures/graph/04깊이우선탐색.js
@@ -0,0 +1,83 @@
+/**
+ * @desc : 깊이 우선 탐색
+ * "시작 정점에서 출발해 동일 경로의 마지막 정점까지 순회하고 다시 반대 방향으로 돌아와 다음 경로를 찾아가는 식으로 진행된다"
+ *
+ * @알고리즘 진행방식
+ * 1. v를 방문했음 표시
+ * 2. 방문하지 않은 v의 인접 정점 w에 대해
+ *  2-1. 정점 w를 방문한다
+ * 3. v를 탐색했음 으로 표시
+ *
+ * 단계별로 재귀 호출을 하여 그때그때 스택에 저장한다
+ */
+
+import { Graph } from "./01그래프.js";
+
+const Colors = {
+  WHITE: 0,
+  GREY: 1,
+  BLACK: 2,
+};
+
+const initializeColor = (vertices) => {
+  const color = {};
+  for (let i = 0; i < vertices.length; i++) {
+    color[vertices[i]] = Colors.WHITE;
+  }
+  return color;
+};
+
+export const depthFirstSearch = (graph, callback) => {
+  const vertices = graph.getVertices();
+  const adjList = graph.getAdjList();
+  const color = initializeColor(vertices);
+
+  for (let i = 0; i < vertices.length; i++) {
+    if (color[vertices[i]] === Colors.WHITE) {
+      depthFirstSearchVisit(vertices[i], color, adjList, callback);
+    }
+  }
+};
+
+// 정점 u를 방문했으니 표시
+function depthFirstSearchVisit(u, color, adjList, callback) {
+  color[u] = Colors.GREY;
+  if (callback) {
+    callback(u); // 방문한 정점을 출력하는 콜백
+  }
+
+  const neighbors = adjList.get(u);
+  for (let i = 0; i < neighbors.length; i++) {
+    const w = neighbors[i];
+    if (color[w] === Colors.WHITE) {
+      depthFirstSearchVisit(w, color, adjList, callback);
+    }
+  }
+  color[u] = Colors.BLACK;
+}
+
+/**
+ * @Test
+ */
+
+const graph = new Graph();
+const Vertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
+for (let i = 0; i < Vertices.length; i++) {
+  graph.addVertex(Vertices[i]);
+}
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("C", "D");
+graph.addEdge("C", "G");
+graph.addEdge("D", "G");
+graph.addEdge("D", "H");
+graph.addEdge("B", "E");
+graph.addEdge("B", "F");
+graph.addEdge("E", "I");
+
+const printNode = (value) => {
+  console.log("탐색한 정점 : ", value);
+};
+
+depthFirstSearch(graph, printNode);
