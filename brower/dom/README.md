fded87a831d6515b0989f317f67e5986bd9042ed
parkgeonwoong <hk77kh@gmail.com>
2023-03-23 22:50:36 +0900

    Perf : 브라우저 렌더링 layout교체 → composite 교체로 개선

--- brower/dom/README.md
+++ brower/dom/README.md
@@ -1,11 +1,41 @@
 ## DOM
 
-브라우저는 HTML Tag 읽어서 ---- DOM Tree 생성 --->  JS Node로 만듬 (document, element ...)
-  
-  
-EventTarget → Node → Element → HTMLElement → HTMLInputElement 
-  
+브라우저는 HTML Tag 읽어서 ---- DOM Tree 생성 ---> JS Node로 만듬 (document, element ...)
+
+EventTarget → Node → Element → HTMLElement → HTMLInputElement
 (모든 노드는 이벤트가 발생할 수 있음)
 
-  
+<br>
+
 ✅ DOM → 브라우저가 읽을 수 있도록 Object 트리로 만들어 나가는 것
+
+<br>
+
+## CSSOM
+
+DOM + CSS = CSSOM
+
+- 구성된 DOM 트리에다가 CSS을 입힌다.
+
+- cascade가 적용이 되어있다.
+
+<br>
+
+Render Tree = DOM + CSSOM
+
+<br>
+
+## 렌더링 과정
+
+브라우저에서 url을 입력하면
+
+request/response(HTML) → loading → scripting(DOM 생성, CSSOM 생성) → Render Tree 생성 → layout → painting → composite
+
+\- 레이아웃은 말그대로 이 정도 위치에 지정.
+
+\- painting은 (비트맵으로) 레이어별(층)로 준비하는 것이다.
+
+- 왜? 그려놓지 않는가? → 성능개선 및 레이아웃 변경시 다시 그리는 것을 방지하기 위해
+
+그렇다면 layout 바뀌는 것이 성능에 최악이다. → layout > painting > composite
+(이에 따른 참고 https://www.lmame-geek.com/css-triggers/)
--- brower/webAPIs/실습/04좌표실습.js
+++ brower/webAPIs/실습/04좌표실습.js
@@ -3,6 +3,10 @@
  * 1. 수평선, 수직선, 타켓점, 좌표 보여줄것
  * 2. mousemove 이벤트
  * 3. 브라우저 안에서 움직임
+ *
+ * @TODO:
+ * - 성능개선이 필요함 → [렌더링과정]에서 top,left는 layout을 바꿔버린다
+ * - 그러기 때문에 composite, painting정도만 바뀌는 것으로 교체해주면 성능이 개선된다. → transform
  */
 
 const vertical = document.querySelector(".vertical");
@@ -10,16 +14,32 @@ const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-// 브라우저 document에서 움직임 감지
-document.addEventListener("mousemove", (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+addEventListener("load", () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
+  // 브라우저 document에서 움직임 감지
+  document.addEventListener("mousemove", (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    // ✅성능개선
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    tag.style.transform = `translate(${x + 20}px, ${y + 20}px)`;
+    tag.innerHTML = `${x}px, ${y}px`;
+
+    /* 
   vertical.style.left = `${x}px`;
   horizontal.style.top = `${y}px`;
   target.style.left = `${x}px`;
   target.style.top = `${y}px`;
   tag.style.left = `${x}px`;
   tag.style.top = `${y}px`;
-  tag.innerHTML = `${x}px, ${y}px`;
+   */
+  });
 });
