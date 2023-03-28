0494d91d9b8793b974f351bee4eb39106c51ce52
parkgeonwoong <hk77kh@gmail.com>
2023-03-28 22:54:02 +0900

    Refactor : DOM 실습 리팩토링

--- brower/dom/실습/index.js
+++ brower/dom/실습/index.js
@@ -1,12 +1,28 @@
 const main = document.querySelector("main");
 const input = document.querySelector("input");
-// const removeBtns = document.querySelectorAll(".remove");
 
 // 생성
 const onInput = (event) => {
+  if (event.target === "") {
+    input.focus();
+    return;
+  }
+  // 추가한 것을 담은 객체 div
+  const item = onCreate(event.target.value);
+
+  // main DOM에 집어넣기
+  main.append(item);
+
+  // input 초기화
+  event.target.value = "";
+  input.focus();
+};
+
+// DOM 생성 함수
+function onCreate(text) {
   const span = document.createElement("span");
   span.setAttribute("class", "context");
-  span.textContent = event.target.value;
+  span.textContent = text;
 
   const button = document.createElement("button");
   button.setAttribute("class", "remove");
@@ -19,20 +35,11 @@ const onInput = (event) => {
 
   div.append(span);
   div.append(button);
+  button.addEventListener("click", () => {
+    main.removeChild(div);
+  });
 
-  main.append(div);
-
-  event.target.value = "";
-
-  button.addEventListener("click", onRemove);
-};
-
-// 삭제
-const onRemove = (event) => {
-  event.target.parentElement.parentElement.parentElement.remove();
-};
+  return div;
+}
 
 input.addEventListener("change", onInput);
-// removeBtns.forEach((btn) => {
-//   btn.addEventListener("click", onRemove);
-// });
