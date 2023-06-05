d219c04cc88cb1c7f2052a8a7c10cc9616513b8e
parkgeonwoong <hk77kh@gmail.com>
2023-06-05 23:17:27 +0900

    Refactor : 필드 클래스 분할

--- brower/event/실습2/src/main.js
+++ brower/event/실습2/src/main.js
@@ -5,20 +5,23 @@
  * 3. 랜덤함수를 생성 -> 랜덤의 범위를 찾아보지 못함
  * 4. 기능별로 함수를 생성해서 호출하는 형식
  * 5. 아이콘을 바꾸는 방법 -> classList.add,remove로 활용
- * 6. 객체 생성시 중복 안되게 field.innerHTML = '' 로 표현
- * 7. 각각의 객체에 이벤트를 넣어주는 것이 아닌 field에 이벤트위임을 해야함
- * 8. event.target에서 target.matches로 조건
+ * 6. 객체 생성시 중복생성 때문에 field.innerHTML = '' 로 표현
+ * 7. 각각의 객체(당근,버그)에 이벤트를 넣어주는 것이 아닌 -> field에 이벤트위임
+ * 8. event.target에서 target.matches 조건으로 당근,버그 구별
+ * ∴ https://github.com/parkgeonwoong/rootjs-JS/blob/849fb0868a8f432a31b72beab43fcbdf806b8e18/brower/event/%EC%8B%A4%EC%8A%B52/main.js
+ *
+ * FIXME:
+ * html을 보고 섹션별로 분할 필요성
+ * 1. 팝업창 클래스로 분할
+ * 2. 필드 클래스로 분할
  */
 
 import PopUp from "./popup.js";
+import Field from "./field.js";
 
-const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
 const GAME_DURATION_SEC = 5;
-
-const field = document.querySelector(".game__field");
-const fieldRect = field.getBoundingClientRect();
 
 const gameBtn = document.querySelector(".game__button");
 const gameTimer = document.querySelector(".game__timer");
@@ -35,12 +38,27 @@ let score = 0;
 let timer = undefined;
 
 const gameFinishBanner = new PopUp();
-
 gameFinishBanner.setClickListener(() => {
   startGame();
 });
 
-field.addEventListener("click", onFieldClick);
+const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+gameField.setClickListener(onItemClick);
+
+function onItemClick(item) {
+  if (!started) {
+    return;
+  }
+  if (item === "carrot") {
+    score++;
+    updateScoreBoard();
+    if (CARROT_COUNT === score) {
+      finishGame(true);
+    }
+  } else if (item === "bug") {
+    finishGame(false);
+  }
+}
 
 gameBtn.addEventListener("click", () => {
   if (started) {
@@ -120,29 +138,8 @@ function updateTimerText(time) {
  */
 function initGame() {
   score = 0;
-  field.innerHTML = "";
   gameScore.innerText = CARROT_COUNT;
-  addItem("carrot", CARROT_COUNT, "../실습/img/carrot.png");
-  addItem("bug", BUG_COUNT, "../실습/img/bug.png");
-}
-
-function onFieldClick(event) {
-  if (!started) {
-    return;
-  }
-  const target = event.target;
-
-  if (target.matches(".carrot")) {
-    target.remove();
-    score++;
-    playSound(carrotSound);
-    updateScoreBoard();
-    if (CARROT_COUNT === score) {
-      finishGame(true);
-    }
-  } else if (target.matches(".bug")) {
-    finishGame(false);
-  }
+  gameField.init();
 }
 
 function playSound(sound) {
@@ -156,28 +153,4 @@ function stopSound(sound) {
 
 function updateScoreBoard() {
   gameScore.innerText = CARROT_COUNT - score;
-}
-
-function addItem(className, count, imgPath) {
-  const x1 = 0;
-  const y1 = 0;
-  const x2 = fieldRect.width - CARROT_SIZE;
-  const y2 = fieldRect.height - CARROT_SIZE;
-
-  for (let i = 0; i < count; i++) {
-    const item = document.createElement("img");
-    item.setAttribute("class", className);
-    item.setAttribute("src", imgPath);
-    item.style.position = "absolute";
-    item.style.cursor = "pointer";
-    const x = randomNumber(x1, x2);
-    const y = randomNumber(y1, y2);
-    item.style.left = `${x}px`;
-    item.style.top = `${y}px`;
-    field.appendChild(item);
-  }
-}
-
-function randomNumber(min, max) {
-  return Math.random() * (max - min) + min;
 }
