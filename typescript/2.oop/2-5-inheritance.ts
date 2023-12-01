c6e1c23e29168df7f8f8523dceaffba71e24ada2
parkgeonwoong <hk77kh@gmail.com>
2023-12-01 17:38:29 +0900

    feat : 상속으로 커피머신 만들기

---
+++ typescript/2.oop/2-5-inheritance.ts
@@ -0,0 +1,78 @@
+{
+  type CoffeeCup = {
+    shots: number;
+    hasMilk: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAMM_PER_SHOT = 7;
+    private coffeeBeans: number = 0;
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) throw new Error("커피 콩은 0개보다 커야해요!");
+      this.coffeeBeans += beans;
+    }
+
+    grindBeans(shots: number) {
+      console.log(`커피 콩 갈고있음... ${shots}샷`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT) {
+        throw new Error("커피 콩이 충분하지 않아요!");
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAMM_PER_SHOT;
+    }
+
+    preheat() {
+      console.log("데우는 중...");
+    }
+
+    clean() {
+      console.log("기계 청소 중...");
+    }
+
+    extract(shots: number): CoffeeCup {
+      console.log(`커피 추출 중... ${shots}샷`);
+      return { shots, hasMilk: false };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CaffeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+
+    private steamMilk() {
+      console.log("우유 스팀 중...");
+    }
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      this.steamMilk();
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  const machine = new CoffeeMachine(23);
+  const latteMachine = new CaffeLatteMachine(23, "SS");
+  const coffee = latteMachine.makeCoffee(1);
+  console.log(coffee);
+  console.log(latteMachine.serialNumber);
+}
