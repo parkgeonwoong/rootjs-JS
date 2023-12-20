c2fdda069258c99bef5f48d1ef827128a2160ecd
parkgeonwoong <hk77kh@gmail.com>
2023-12-20 15:42:26 +0900

    feat : 에러처리 핸들링 위치에 대한 생각

---
+++ typescript/7.exception/7-2-handling.ts
@@ -0,0 +1,31 @@
+class NetworkClient {
+  tryConnect(): void {
+    throw new Error("네트워크 연결이 끊어졌습니다.");
+  }
+}
+
+class UserService {
+  constructor(private client: NetworkClient) {}
+
+  login() {
+    this.client.tryConnect();
+  }
+}
+
+class App {
+  constructor(private userService: UserService) {}
+  run() {
+    // login에서 할줄 알았지만 별도의 처리할 기능을 삼기 어렵다.
+    // app에서 유저에게 따로 처리할 기능을 제공할 수 있기 때문에 여기서 try-catch
+    try {
+      this.userService.login();
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+}
+
+const client = new NetworkClient();
+const service = new UserService(client);
+const app = new App(service);
+app.run();
