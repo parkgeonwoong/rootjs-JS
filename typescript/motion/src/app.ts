b3d64ebb9066c14d823d9d5ce04bd09249f044ff
parkgeonwoong <hk77kh@gmail.com>
2024-01-23 17:02:04 +0900

    feat : image 컴포넌트 구현

--- typescript/motion/src/app.ts
+++ typescript/motion/src/app.ts
@@ -1,3 +1,4 @@
+import { ImageComponent } from './components/page/item/image.js';
 import { PageComponent } from './components/page/page.js';
 
 class App {
@@ -6,6 +7,9 @@ class App {
   constructor(appRoot: HTMLElement) {
     this.page = new PageComponent();
     this.page.attachTo(appRoot);
+
+    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300');
+    image.attachTo(appRoot, 'beforeend');
   }
 }
 
