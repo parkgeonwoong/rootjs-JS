c1d92b8867572ed4451f64c692fc179c7f082f0c
parkgeonwoong <hk77kh@gmail.com>
2024-01-25 13:09:33 +0900

    refactor : base 컴포넌트를 상속으로 리팩토링

--- typescript/motion/dist/components/page/item/image.js
+++ typescript/motion/dist/components/page/item/image.js
@@ -1,20 +1,16 @@
-export class ImageComponent {
+import { BaseComponent } from '../../component.js';
+export class ImageComponent extends BaseComponent {
     constructor(title, url) {
-        const template = document.createElement('template');
-        template.innerHTML = `<section class="image">
-          <div class="image__holder">
-            <img class="image__thumbnail">
-            <p class="image__title"></p>
-          </div>
-        </section>`;
-        this.element = template.content.firstElementChild;
+        super(`<section class="image">
+            <div class="image__holder">
+                <img class="image__thumbnail">
+                <p class="image__title"></p>
+            </div>
+           </section>`);
         const imageElement = this.element.querySelector('.image__thumbnail');
         imageElement.src = url;
         imageElement.alt = title;
         const titleElement = this.element.querySelector('.image__title');
         titleElement.textContent = title;
-    }
-    attachTo(parent, position = 'afterbegin') {
-        parent.insertAdjacentElement(position, this.element);
     }
 }
