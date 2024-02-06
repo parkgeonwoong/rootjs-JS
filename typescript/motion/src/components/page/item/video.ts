653cbd37d9cb8e205f1571bb01eb91c23ee0e90f
parkgeonwoong <hk77kh@gmail.com>
2024-02-06 16:31:34 +0900

    fix : 드랍시 페이지 아래 모든 요소 인식안되게 수정

--- typescript/motion/src/components/page/item/video.ts
+++ typescript/motion/src/components/page/item/video.ts
@@ -3,10 +3,9 @@ import { BaseComponent } from '../../component.js';
 export class VideoComponent extends BaseComponent<HTMLElement> {
   constructor(title: string, url: string) {
     super(`<section class="video">
-                <div class="video__player"><iframe class="video__iframe"></iframe>
-                <h3 class="page-item__title video__title"></h3>
-                </div>
-            </section>`);
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="page-item__title video__title"></h3>
+          </section>`);
 
     const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement;
     iframe.src = this.convertToEmbeddedURL(url);
--- typescript/motion/src/components/page/page.ts
+++ typescript/motion/src/components/page/page.ts
@@ -11,6 +11,7 @@ type OnDragStateListener<T extends Component> = (target: T, state: DragState) =>
 interface SectionContainer extends Component, Composable {
   setOnCloseListener(listener: OnCloseListener): void;
   setOnDragStateListener(listener: OnDragStateListener<SectionContainer>): void;
+  muteChildren(state: 'mute' | 'unmute'): void;
 }
 
 type SectionContainerConstructor = {
@@ -80,9 +81,18 @@ export class PageItemComponent extends BaseComponent<HTMLElement> implements Sec
   setOnDragStateListener(listener: OnDragStateListener<PageItemComponent>) {
     this.dragStateListener = listener;
   }
+
+  muteChildren(state: 'mute' | 'unmute') {
+    if (state === 'mute') {
+      this.element.classList.add('mute-children');
+    } else {
+      this.element.classList.remove('mute-children');
+    }
+  }
 }
 
 export class PageComponent extends BaseComponent<HTMLUListElement> implements Composable {
+  private children = new Set<SectionContainer>();
   private dragTarget?: SectionContainer;
   private dropTarget?: SectionContainer;
 
@@ -120,24 +130,36 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
     item.attachTo(this.element, 'beforeend');
     item.setOnCloseListener(() => {
       item.removeFrom(this.element);
+      this.children.delete(item);
     });
+    this.children.add(item);
     item.setOnDragStateListener((target: SectionContainer, state: DragState) => {
       switch (state) {
         case 'start':
           this.dragTarget = target;
+          this.updateSection('mute');
           break;
         case 'end':
           this.dragTarget = undefined;
+          this.updateSection('unmute');
           break;
         case 'enter':
+          console.log('enter', target);
           this.dropTarget = target;
           break;
         case 'leave':
+          console.log('leave', target);
           this.dropTarget = undefined;
           break;
         default:
           throw new Error(`지원하지 않는 상태: ${state}`);
       }
+    });
+  }
+
+  private updateSection(state: 'mute' | 'unmute') {
+    this.children.forEach((section: SectionContainer) => {
+      section.muteChildren(state);
     });
   }
 }
